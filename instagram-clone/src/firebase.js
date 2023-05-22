a53a8b415ad7857d7dc47ee8d5f0fef71cb798c9
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-05-22 18:52:54 +0530

    Create firebase.js

---
+++ instagram-clone/src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCNNq8u9pSxdmGVUK4s6ticn0M6h2u7oVg",
+    authDomain: "instagram-clone-react-d8e18.firebaseapp.com",
+    projectId: "instagram-clone-react-d8e18",
+    storageBucket: "instagram-clone-react-d8e18.appspot.com",
+    messagingSenderId: "670347313301",
+    appId: "1:670347313301:web:2fcf48520dfdc38ac75b9c",
+    measurementId: "G-3L8WTQ2EYD"
+  };
+
+//export 
