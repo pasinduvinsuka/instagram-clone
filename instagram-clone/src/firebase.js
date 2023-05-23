e792eb9a1f1bde594e671f336a74b3a3cac59282
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-05-23 07:59:46 +0530

    added firebase connection

--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -1,12 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCNNq8u9pSxdmGVUK4s6ticn0M6h2u7oVg",
-    authDomain: "instagram-clone-react-d8e18.firebaseapp.com",
-    projectId: "instagram-clone-react-d8e18",
-    storageBucket: "instagram-clone-react-d8e18.appspot.com",
-    messagingSenderId: "670347313301",
-    appId: "1:670347313301:web:2fcf48520dfdc38ac75b9c",
-    measurementId: "G-3L8WTQ2EYD"
-  };
+import firebase from "firebase";
 
-//export 
+const firebaseApp = firebase.initializeApp( {
+  apiKey: "AIzaSyCNNq8u9pSxdmGVUK4s6ticn0M6h2u7oVg",
+  authDomain: "instagram-clone-react-d8e18.firebaseapp.com",
+  projectId: "instagram-clone-react-d8e18",
+  storageBucket: "instagram-clone-react-d8e18.appspot.com",
+  messagingSenderId: "670347313301",
+  appId: "1:670347313301:web:2fcf48520dfdc38ac75b9c",
+  measurementId: "G-3L8WTQ2EYD"
+});
+
+// Initialize Firebase
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export{db, auth, storage};
